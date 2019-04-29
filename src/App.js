8b2e2ea5942e7a262403a819d34f86ab31c9f3ed
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-04-29 12:04:53 +0700

    get API from zomatos, and show it with DOM

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React from "react"
-// import axios from "axios"
+import axios from "axios"
 
 import { Container, Card, Image } from "semantic-ui-react"
 
+const API_STRING = `https://developers.zomato.com/api/v2.1/search?entity_id=74&entity_type=city`
+
+const API_CONFIGURATION = {
+  headers: {
+    "user-key": "6ff1cfa18e834e1de8af7b7920acb0a1"
+  }
+}
+
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      restaurants: []
+      // restaurantNames: [],
+      // restaurantThumbnails: []
+      // restaurant
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_STRING, API_CONFIGURATION)
+      .then(response => {
+        // console.log(response.data.restaurants)
+        this.setState({
+          restaurants: response.data.restaurants
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
-    return (
-      <div>
-        <Card>
-          <Image src="/" />
-          <Card.Content>
-            <Card.Header>Sushi Hiro</Card.Header>
-            <Card.Meta>Jl. Senopati, Jakarta</Card.Meta>
-            <Card.Description>Cheap sushi makes happy</Card.Description>
-          </Card.Content>
-        </Card>
-      </div>
-    )
+    const restaurants = this.state.restaurants.map((item, index) => {
+      console.log(item.restaurant)
+      return (
+        <div>
+          <Card>
+            <Image src="/" />
+            <Card.Content>
+              <Card.Header>{item.restaurant.name}</Card.Header>
+              <Card.Meta>Jl. Senopati, Jakarta</Card.Meta>
+              <Card.Description>Cheap sushi makes happy</Card.Description>
+            </Card.Content>
+          </Card>
+        </div>
+      )
+    })
+
+    return <div>{restaurants}</div>
   }
 }
